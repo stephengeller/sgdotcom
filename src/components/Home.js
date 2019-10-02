ba55a5c283d9b240c45d29d6575a3e4f44ec923f
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-10-02 14:56:43 +0900

    Feat: Add MVP Parallax landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import Parralax from "./Parallax";
 
 export default class HomePage extends Component {
   render() {
-    return <div>You are home!</div>;
+    return <div>Home.</div>;
   }
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Parralax from "./Parallax";
 
 export default class Landing extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Parralax />
+      </div>
+    );
   }
 }
