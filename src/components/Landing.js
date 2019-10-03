3852126a07c0b88f1dbb000b6403ac3d60ea2bd4
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-10-03 20:45:04 +0900

    Chore: Improve Carousel...slightly

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,77 +1,44 @@
 import React from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 
-const rowStyle = {
-  height: "100%",
-  width: "100%",
-  textAlign: "center",
-  display: "flex",
-  alignItems: "center"
-};
+function carouselItem(image, header, sub, alt = "carousel-image") {
+  return (
+    <Carousel.Item>
+      <Image
+        className="d-block w-100"
+        src={image}
+        fluid
+        alt={alt}
+        style={{
+          display: "block",
+          width: "100%",
+          height: "auto"
+        }}
+      />
+      <Carousel.Caption>
+        <h3>{header}</h3>
+        <p>{sub}</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  );
+}
 
-const LandingCarousel = () => {
+const Landing = () => {
   return (
     <Carousel>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="img/StephenJapan2.jpg"
-          fluid
-          alt="First slide"
-          style={{}}
-        />
-        <Carousel.Caption>
-          <h3>{"Hi, I'm Stephen."}</h3>
-          <p>I write code for a living.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="img/AfterlightImage.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>I also take photos.</h3>
-          <p>Mainly with my iPhone.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {carouselItem(
+        "img/StephenJapan2.jpg",
+        "Hi, I'm Stephen.",
+        "I write code for a living."
+      )}
+      {carouselItem(
+        "img/AfterlightImage.jpg",
+        "I like photography",
+        "I take pictures with an iPhone XS"
+      )}
     </Carousel>
   );
 };
 
-const Landing = () => {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        left: "0",
-        padding: "80px",
-        width: "100%"
-      }}
-    >
-      <Container>
-        <Row style={rowStyle}>
-          <Col sm>Software Developer</Col>
-          <Col sm>
-            <img
-              alt="testing"
-              src="img/stephenCartoon.png"
-              width="100%"
-              height="100%"
-              className="d-inline-block align-top"
-            />
-          </Col>
-          <Col sm>Site under construction!</Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
-
-export default LandingCarousel;
+export default Landing;
