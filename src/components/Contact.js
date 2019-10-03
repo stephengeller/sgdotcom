c3f158beeafdefa6d9f8aaeacfd6b023e939e5b9
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-10-04 00:09:39 +0900

    Feat: Add contact icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,29 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import { FaTwitter, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import * as URLS from "../constants/urls";
 
 const Contact = () => {
   return (
     <div>
-      <Container>
-        <h1>Contact Details</h1>
-        <p>Email</p>
-        <p>GitHub</p>
+      <Container
+        style={{
+          paddingTop: "20px",
+          textAlign: "center"
+        }}
+      >
+        <a href={URLS.GITHUB}>
+          <FaGithub className="contact-icon" />
+        </a>
+        <a href={URLS.TWITTER}>
+          <FaTwitter className="contact-icon" />
+        </a>
+        <a href={URLS.INSTAGRAM}>
+          <FaInstagram className="contact-icon" />
+        </a>
+        <a href={URLS.LINKEDIN}>
+          <FaLinkedin className="contact-icon" />
+        </a>
       </Container>
     </div>
   );
