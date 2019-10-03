9d681ec4eb755fc8f40778662398ef64ef1c83be
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-10-04 00:04:45 +0900

    Chore: Add content to About

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-
-const info = "I like killing dreams and making memes. More info to come.";
+import * as URLS from "../constants/urls";
 
 export const About = () => {
   return (
     <div>
       <Container>
         <h1>About</h1>
-        <div>{info}</div>
+        <div>
+          I am a London-based software developer, proficient in web-development
+          languages and frameworks such as Ruby on Rails, Javascript and Node.
+          <br />
+          <br />
+          Through my time at <a href="https://makers.tech"> Makers Academy</a>,
+          I have learnt to write my code using Test-Driven Development
+          techniques ensuring that I produce clean, efficient code for any
+          project.
+          <br />
+          <br />
+          You can see my full CV on my <a href={`${URLS.GITHUB}/CV`}>
+            GitHub{" "}
+          </a>{" "}
+          page.
+        </div>
       </Container>
     </div>
   );
