11303747259ecf8b31d3913f9cd16688a3866f6c
Stephen Geller <stephen.gb.geller+git@gmail.com>
2019-12-28 00:31:59 +0000

    Replace constants to JSON

--- src/constants/urls.js
+++
@@ -1,5 +0,0 @@
-export let EMAIL = "mailto:stephen.gb.geller+contact@gmail.com";
-export let TWITTER = "https://twitter.com/_stephengeller";
-export const GITHUB = "https://github.com/stephengeller";
-export const INSTAGRAM = "https://instagram.com/stephengeorgebaker";
-export const LINKEDIN = "https://www.linkedin.com/in/stephen-geller/";
