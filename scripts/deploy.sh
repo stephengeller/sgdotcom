893c16d8e89f4a8226fd775ec1cb60f0d512d975
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-05-09 18:38:51 +0100

    better deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-set -xe
+RED="\033[31m"
+PLAIN="\033[0m"
 
+command -v firebase
+
+if [[ $? -ne 0 ]]; then
+    echo -e "${RED}Firebase CLI not found, please install globally to deploy."
+    echo -e "$ yarn gloabl add firebase-tools${PLAIN}"
+    exit 1
+fi
+
+echo "Building..."
 yarn build
+
+echo "Deploying..."
 yarn deploy
+
+echo "Deployed."
