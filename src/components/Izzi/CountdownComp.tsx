8fac70f3793bf9254c4e95449523660efff40468
stephengeller <stephen.geller@hotmail.com>
2020-05-19 22:21:19 +0100

    improve style

--- src/components/Izzi/CountdownComp.tsx
+++ src/components/Izzi/CountdownComp.tsx
@@ -3,16 +3,24 @@ import { Component } from "react";
 import { css, jsx } from "@emotion/core";
 import Countdown from "react-countdown";
 
-const parent = css({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-  padding: "0",
-  margin: "0",
-  backgroundColor: "black",
-  color: "white"
-});
+const styles = {
+  parent: css({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    height: "100vh",
+    padding: "10",
+    margin: "0",
+    backgroundColor: "#eae2b7",
+    color: "#003049",
+    fontSize: "4.5vw",
+    fontFamily: "helvetica"
+  }),
+  countdown: {
+    number: { color: "#d62828" }
+  }
+};
 
 const renderer: ({
   days,
@@ -33,16 +41,17 @@ const renderer: ({
     return (
       <div>
         <div>
-          <span style={{ color: "lightgreen" }}>{days}</span> days
+          <span style={styles.countdown.number}>{days}</span> days
         </div>
         <div>
-          <span style={{ color: "lightgreen" }}>{hours}</span> hours
+          <span style={styles.countdown.number}>{hours}</span> hours
         </div>
         <div>
-          <span style={{ color: "lightgreen" }}>{minutes}</span> minutes
+          <span style={styles.countdown.number}>{minutes}</span> minutes
         </div>
+        and
         <div>
-          <span style={{ color: "lightgreen" }}>{seconds}</span> seconds
+          <span style={styles.countdown.number}>{seconds}</span> seconds
         </div>
       </div>
     );
@@ -57,8 +66,17 @@ export class CountdownComp extends Component<{}, {}> {
 
   render(): JSX.Element {
     return (
-      <div css={parent}>
+      <div css={styles.parent}>
+        <div style={{ padding: "10px" }}>{"Hey bub!"}</div>
+        <div>
+          {"I hereby "}
+          <strong>promise</strong>
+          {" that I'll be with you in"}
+        </div>
         <Countdown renderer={renderer} date={new Date(2020, 10, 19)} />
+        <div style={{ padding: "20px", color: "#d62828" }}>
+          {"I love you so so so so so much"}
+        </div>
       </div>
     );
   }
